840e51ec8761309ba8b6751f2d8caaf477d7b41f
leolee215 <leolee85215@gmail.com>
2025-03-26 23:34:13 +0800

    modify CORS

--- index.js
+++ index.js
@@ -30,14 +30,20 @@ app.use(session({
     saveUninitialized: true
 }))
 
-app.use(cors())
+// app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(flash())
 
 // app.get('/', givingController.index)
-
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(200);
+});
+  
 app.post('/api/payment', async (req, res, next) => {
     // res.setHeader('Access-Control-Allow-Origin', '*');
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
